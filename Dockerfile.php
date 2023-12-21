76e1d738e9e6bd56b039ae1883053cd27e879013
Michael Mayer <michael.mayer@unibw.de>
2023-12-21 18:46:44 +0100

    fix docker environment

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,9 +1,9 @@
 FROM php:8.2-fpm-alpine
-COPY . /var/www/html/
+COPY . /var/www/html/public
 
 RUN apk add --no-cache git libzip-dev zip \
     && docker-php-ext-install zip \
-    && cd /var/www/html \
+    && cd /var/www/html/public \
     && chmod +x composer_install.sh && ./composer_install.sh \ 
     && mv composer.phar /usr/local/bin/composer \
     && composer install --no-cache \
